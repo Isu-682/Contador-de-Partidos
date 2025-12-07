ba53d63d4c16281dc2eedb87c71c4a32e7bcf3e4
Isu-682 <padillamachayariana@gmail.com>
2025-12-07 01:31:55 -0600

    Agregar archivos de proyecto.

---
+++ Contador de Partidos/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Contador_de_Partidos")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Contador_de_Partidos.Pages")]
